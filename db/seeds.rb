f6e22052901cf854a4769dad07e89722674c9b2d
Bianca <ing_biank@hotmail.com>
2018-07-23 12:52:50 +0200

    Updates Seeds, creates custom scopes and creates angula_items page for portfolios

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+3.times do |topic|
+  Topic.create!(
+    title: "topic #{topic}"
+  )
+end
+
+puts "3 topics created"
 
 10.times do |blog|
   Blog.create!(
@@ -15,7 +22,8 @@
     et quas molestias excepturi sint occaecati
     cupiditate non provident, similique sunt in
     culpa qui officia deserunt mollitia animi,
-    id est laborum et dolorum fuga."
+    id est laborum et dolorum fuga.",
+    topic_id: Topic.last.id
   )
 end
 
@@ -30,10 +38,26 @@ end
 
 puts "5 skills created"
 
-9.times do |prortfolio_item|
+8.times do |prortfolio_item|
   Portfolio.create!(
     title: "Porfolio title #{prortfolio_item}",
-    subtitle: "My great service",
+    subtitle: "Ruby on rails",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+    culpa qui officia deserunt mollit anim id est laborum.",
+    main_image: "http://via.placeholder.com/600x400",
+    thumb_image: "http://via.placeholder.com/350x200"
+  )
+end
+
+1.times do |prortfolio_item|
+  Portfolio.create!(
+    title: "Porfolio title #{prortfolio_item}",
+    subtitle: "Angular",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
     sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
