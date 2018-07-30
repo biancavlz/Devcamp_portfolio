90230ecc05f10f3707f5f102e65909e725aa2ebe
Bianca <ing_biank@hotmail.com>
2018-07-30 16:30:11 +0200

    Change page title

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -6,7 +6,7 @@ module DefaultPageContent
   end
 
   def set_page_defaults
-    @page_title = "DevcampPortfolio | My Portfolio Website"
+    @page_title = "My Portfolio Website"
     @seo_keywords = "Bianca Velazquez Portfolio"
   end
 end
