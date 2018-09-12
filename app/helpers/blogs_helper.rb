d967656632086778e0715c2f0273a980dedac43d
Bianca <ing_biank@hotmail.com>
2018-09-12 18:39:30 +0200

    configures coderay

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -2,4 +2,10 @@ module BlogsHelper
   def gravatar_helper user
     image_tag "https://www.gravatar.com/avatar/#{Digest::MD5::hexdigest(user.email)}", width: 64, class: "card-img-left"
   end
+
+  class CodeRayify < Redcarpet::Render::HTML
+    def block_code(code, language)
+      CodeRay.scan(code, language).div
+    end
+  end
 end
