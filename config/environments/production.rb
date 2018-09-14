be6455e827cc865b4a73c3a07f9f0a51090ae31b
Bianca <ing_biank@hotmail.com>
2018-09-14 19:41:22 +0200

    Integrates production call for using action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_cable.allowed_request_origins = ['http://bv-dev-portfolio.herokuapp.com', 'https://bv-dev-portfolio.herokuapp.com']
+  config.action_cable.url = "wss://bv-dev-portfolio.herokuapp.com/cable"
 end
